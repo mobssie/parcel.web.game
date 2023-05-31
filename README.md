fcad3ffab9d1526a6d64bbe1c62ee93d18b9d44d
정주리 <mobssie.jung@gmail.com>
2023-06-01 01:39:43 +0900

    장애물

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-# parcel.web.game
-개발환경 parcel-bundler / Vanilla JS web game
+# parcel.web.game  
+개발환경 parcel-bundler / Vanilla JS web game  
 
-# 개발 시작
-npm init -y
-npm install --save-dev parcel-bundler
+# 개발 시작  
+npm init -y  
+npm install --save-dev parcel-bundler  
 
-# 폴더 구조
-touch index.html
-mkdir src
-cd src
-touch index.js
-cd ..
+# 폴더 구조  
+touch index.html  
+mkdir src  
+cd src  
+touch index.js  
+cd ..  
 
--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ gameMessage.addEventListener("click", start);
 document.addEventListener("keydown", pressOn);
 document.addEventListener("keyup", pressOff);
 let keys = {};
-let bird = document.createElement("div");
-let wing = document.createElement("div");
 let player = {
   x: 0,
   y: 0,
@@ -17,14 +15,22 @@ let player = {
   score: 0,
   inplay: false
 };
+let pipe = {
+  startPos : 0,
+  spaceBetweenRow : 0,
+  spaceBetweenCol : 0,
+  pipeCount : 0,
+}
 
 function start() {
   // 시작할때 값 초기화
   player.inplay = true;
   player.score = 0;
-
+  gameArea.innerHTML = "";
   gameMessage.classList.add("hide");
   startBtn.classList.add("hide");
+  let bird = document.createElement("div");
+  let wing = document.createElement("div");
   bird.setAttribute("class", "bird");
   wing.setAttribute("class", "wing");
   wing.pos = 15;
@@ -33,11 +39,38 @@ function start() {
   gameArea.appendChild(bird);
   player.x = bird.offsetLeft;
   player.y = bird.offsetTop;
+  pipe.startPos = 0;
+  pipe.spaceBetweenRow = 400;
+  pipe.pipeCount = Math.floor(gameArea.offsetWidth / pipe.spaceBetweenRow);
+  for(let i = 0; i < pipe.pipeCount; i++) {
+    makePipe(pipe.startPos * pipe.spaceBetweenRow);
+    pipe.startPos++;
+  }
   window.requestAnimationFrame(playGame);
+}
+
+function makePipe(pipePos) {
+  let totalHeight = gameArea.offsetHeight;
+  let totalWidth = gameArea.offsetWidth;
+  let pipeUp = document.createElement("div");
+  pipeUp.classList.add("pipe");
+  pipeUp.height = Math.floor(Math.random() * 350)
+  pipeUp.style.height = pipeUp.height + "px";
+  pipeUp.style.left = totalWidth + pipePos + "px";
+  pipeUp.x = totalWidth + pipePos + "px";
+  pipeUp.style.top = "0px";
+  pipeUp.style.backgroundColor = "red"
+
+  gameArea.appendChild(pipeUp);
+
+  pipe.spaceBetweenCol = Math.floor(Math.random() * 250) + 150;
+
 }
 
 function playGame() {
   if (player.inplay) {
+    let bird = document.querySelector(".bird")
+    let wing = document.querySelector(".wing")
     let move = false;
     if (keys.ArrowLeft && player.x > 0) {
       player.x -= player.speed;
