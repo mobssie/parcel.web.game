d44af650a93b1f703c8ffd4a248815ac9fae3e11
정주리 <mobssie.jung@gmail.com>
2023-05-31 13:01:43 +0900

    web game

---
+++ src/index.js
@@ -0,0 +1,100 @@
+import "./styles.css";
+
+const score = document.querySelector(".score");
+const startBtn = document.querySelector(".startBtn");
+const gameArea = document.querySelector(".gameArea");
+const gameMessage = document.querySelector(".gameMessage");
+
+startBtn.addEventListener("click", start);
+gameMessage.addEventListener("click", start);
+document.addEventListener("keydown", pressOn);
+document.addEventListener("keyup", pressOff);
+let keys = {};
+let bird = document.createElement("div");
+let wing = document.createElement("div");
+let player = {
+  x: 0,
+  y: 0,
+  speed: 2,
+  score: 0,
+  inplay: false
+};
+
+function start() {
+  // 시작할때 값 초기화
+  player.inplay = true;
+  player.score = 0;
+
+  gameMessage.classList.add("hide");
+  startBtn.classList.add("hide");
+  bird.setAttribute("class", "bird");
+  wing.setAttribute("class", "wing");
+  wing.pos = 15;
+  wing.style.top = wing.pos + "px";
+  bird.appendChild(wing);
+  gameArea.appendChild(bird);
+  player.x = bird.offsetLeft;
+  player.y = bird.offsetTop;
+  window.requestAnimationFrame(playGame);
+}
+
+function playGame() {
+  if (player.inplay) {
+    let move = false;
+    if (keys.ArrowLeft && player.x > 0) {
+      player.x -= player.speed;
+      move = true;
+    }
+    if (keys.ArrowRight && player.x < gameArea.offsetWidth - bird.offsetWidth) {
+      player.x += player.speed;
+      move = true;
+    }
+    if ((keys.ArrowUp || keys.Space) && player.y > 0) {
+      player.y -= player.speed * 5;
+      move = true;
+    }
+    if (
+      keys.ArrowDown &&
+      player.y < gameArea.offsetHeight - bird.offsetHeight
+    ) {
+      player.y += player.speed;
+      move = true;
+    }
+    if (move) {
+      wing.pos = wing.pos === 15 ? 25 : 15;
+      wing.style.top = wing.pos + "px";
+    }
+    player.y += player.speed * 2;
+
+    // 새가 게임 밖으로 나가면 게임오버
+    if (player.y > gameArea.offsetHeight) {
+      console.log("game over");
+      playGameOver();
+    }
+
+    bird.style.left = player.x + "px";
+    bird.style.top = player.y + "px";
+    window.requestAnimationFrame(playGame);
+    player.score++;
+    score.innerText = "SCORE : " + player.score;
+  }
+}
+function playGameOver() {
+  player.inplay = false;
+  gameMessage.classList.remove("hide");
+  gameMessage.innerHTML =
+    "GAME OVER<br/> 당신의 점수는" +
+    player.score +
+    "점 입니다. <br/> 다시 시작하려면 여기를 누르세요.";
+}
+function pressOn(e) {
+  console.log(e.code);
+  keys[e.code] = true;
+  console.log(keys);
+}
+
+function pressOff(e) {
+  console.log(e.code);
+  keys[e.code] = false;
+  console.log(keys);
+}
